d373c135301ab5eea155500df96d5df615209fa5
Cristian Soto <101820809+CristianSz2003@users.noreply.github.com>
2024-08-01 11:04:01 -0500

    Add files via upload

---
+++ SOLID/src/main/EmployeeManagement.java
@@ -0,0 +1,6 @@
+package com.example.demo.unsolid;
+
+public interface EmployeeManagement {
+    void addEmployee(String name);
+    void removeEmployee(String name);
+}
---
+++ SOLID/src/main/saveEmployee.java
@@ -0,0 +1,6 @@
+package com.example.demo.unsolid;
+
+public interface saveEmployee {
+
+    void saveToFile (String name);
+}
